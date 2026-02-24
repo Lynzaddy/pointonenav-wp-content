6c3bb15d7ae4e643d279e760616fea930c69087c
Matt Olson <matt@lynzaddy.com>
2026-02-23 21:23:35 -0600

    Push of the alert bar to get to staging

--- plugins/pointone-site-alert-bar/assets/site-alert-bar.js
+++ plugins/pointone-site-alert-bar/assets/site-alert-bar.js
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         }
     } catch (e) {
-        // Storage blocked? Fail open (bar shows).
+        // Storage blocked? Fail open (bar shows)
     }
 
     const closeBtn = bar.querySelector(".site-alert-bar__close");
--- plugins/pointone-site-alert-bar/site-alert-bar.php
+++ plugins/pointone-site-alert-bar/site-alert-bar.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Point One Nav - Site Alert Bar (ACF + Elementor)
  * Description: Date-based site-wide alert bar with dismiss (localStorage) and ACF fields registered in code.
- * Version: 1.0.2
+ * Version: 1.0.3
  * Author: Point One Navigation
  * License: GPLv2 or later
  */
