19aecf56644d1b0260c30a94639b305232322712
Matt Olson <matt@lynzaddy.com>
2026-03-05 13:58:46 -0600

    First push of the Events plugin

---
+++ plugins/pointone-events/pointone-events.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * Plugin Name: Point One Events
+ * Description: Custom Events system for Point One including CPT, ACF fields, admin indicators, and Elementor helpers.
+ * Version: 1.0
+ */ 
